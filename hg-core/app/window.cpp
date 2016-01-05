4e269960d6056b102e6a2bf21c16bad27515512a
liu song <hanliumaozhi@gmail.com>
2016-01-06 00:28:06 +0800

    test

---
+++ hg-core/app/window.cpp
@@ -0,0 +1,56 @@
+//
+// Created by han on 16-1-5.
+//
+
+#include <iostream>
+#include "window.h"
+
+namespace hg{
+    namespace app{
+        window::window(const char *title, int weight, int height) {
+            m_Title = title;
+            m_Width = weight;
+            m_Height = height;
+            if(!Init()){
+                std::cout<<"init error"<<std::endl;
+            };
+        }
+
+        window::~window() {
+            glfwTerminate();
+        }
+
+        bool window::Init() {
+            if(!glfwInit()){
+                return false;
+            }
+            m_Windows = glfwCreateWindow(m_Width, m_Height, m_Title, NULL, NULL);
+            if(!m_Windows){
+                glfwTerminate();
+                return false;
+            }
+
+            glfwMakeContextCurrent(m_Windows);
+            return true;
+
+        }
+
+        bool window::isClosed() const {
+            return glfwWindowShouldClose(m_Windows) == 1;
+        }
+
+        void window::Update(){
+            if(!m_Is_close){
+                m_Is_close = isClosed();
+                glfwSwapBuffers(m_Windows);
+
+                glfwPollEvents();
+            }
+
+        }
+
+        void window::Clear() const {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        }
+    }
+}
---
+++ hg-core/app/window.h
@@ -0,0 +1,47 @@
+//
+// Created by han on 16-1-5.
+//
+
+#ifndef HG_WINDOW_H
+#define HG_WINDOW_H
+
+#include <GLFW/glfw3.h>
+
+namespace hg {
+    namespace app {
+
+        class window {
+            private:
+
+                GLFWwindow* m_Windows;
+                const char* m_Title;
+                int m_Width;
+                int m_Height;
+
+                bool m_Is_close;
+
+            public:
+                window(const char* title, int weight, int height);
+                ~window();
+                void Clear() const;
+                void Update();
+                bool isClosed() const;
+
+                inline int GetWidth() const { return m_Width; };
+                inline int GetHeight() const { return m_Height; };
+
+            private:
+
+                bool Init();
+
+
+
+        };
+
+
+
+    }
+}
+
+
+#endif //HG_WINDOW_H
